f6313bde89396b12d104872e3f0edac3221b0255
Akilan2710 <akilan.tmlzs@gmail.com>
2026-05-31 08:07:06 +0000

    Sync LeetCode submission - Two Sum (cpp)

--- my-folder/problems/two_sum/solution.cpp
+++ my-folder/problems/two_sum/solution.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> num_map;
-        int n = nums.size();
-
-        for (int i = 0; i < n; i++) {
-            int complement = target - nums[i];
-            if (num_map.count(complement)) {
-                return {num_map[complement], i};
+        unordered_map<int,int> indexnum;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            int c=target-nums[i];
+            if(indexnum.count(c)){
+                return {indexnum[c],i};
             }
-            num_map[nums[i]] = i;
+            indexnum[nums[i]]=i;
         }
-
-        return {}; // Not reachable
+        return{};
     }
 };
