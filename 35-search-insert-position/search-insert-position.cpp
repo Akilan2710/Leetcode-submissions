e61b52de2fff599b1cce0fc1f129663c0039e682
Akilan2710 <akilan.tmlzs@gmail.com>
2026-07-16 11:05:12 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (43.25%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int c=0;
+        for(int n:nums){
+            if(n>=target){
+                break;
+            }
+            c++;
+        }
+        return c;
+    }
+};
